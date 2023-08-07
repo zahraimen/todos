3d6b375a858ca491e40bcc8456108ed59a14060a
zahra imen <zahraimen91@gmail.com>
2023-08-07 13:57:55 +0330

    add rest_framework config to seeting

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -45,6 +45,13 @@ INSTALLED_APPS = [
     'todos',
 ]
 
+#restframework config
+REST_FRAMEWORK={
+'DEFAULT_PERMISSION_CLASSES': [
+'rest_framework.permissions.AllowAny',
+]
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
